1256d3c76054186e96d61acb6ba28b10db8a8ffb
minky5004 <minky5004@gmail.com>
2026-04-01 17:12:35 +0900

    feat:init setting

---
+++ src/main/java/com/example/cafebackendproject/CafeBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafebackendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeBackendProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeBackendProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cafebackendproject/CafeBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafebackendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeBackendProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
