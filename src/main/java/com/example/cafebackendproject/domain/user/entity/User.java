4167be7cbd46f9a545af911cb0059b72e1bdd7f4
minky5004 <minky5004@gmail.com>
2026-04-04 19:15:02 +0900

    feat: auth관련 에러코드 추가 í및 User엔티티추가

---
+++ src/main/java/com/example/cafebackendproject/domain/user/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.cafebackendproject.domain.user.entity;
+
+import com.example.cafebackendproject.domain.user.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    // 이메일은 로그인 ID로 사용, 중복 불가
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole userRole;
+}
---
+++ src/main/java/com/example/cafebackendproject/domain/user/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.cafebackendproject.domain.user.enums;
+
+public enum UserRole {
+    USER, ADMIN
+}
